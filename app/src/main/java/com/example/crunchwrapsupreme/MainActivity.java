5cb4bb1c14537cd3b3ac8a03200b02fa7e21af4f
Nickalus12 <NPBrewer@student.fullsail.edu>
2023-01-19 14:11:34 -0600

    [BUG FIX] Added Button to open new page.

--- app/src/main/java/com/example/crunchwrapsupreme/MainActivity.java
+++ app/src/main/java/com/example/crunchwrapsupreme/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.crunchwrapsupreme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button helpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helpButton = findViewById(R.id.helpButton);
+
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Help_Page.class);
+                startActivity(intent);
+            }
+        });
     }
 }
