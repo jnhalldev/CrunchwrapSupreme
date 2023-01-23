23703c0bd89cb1fc7017875c06144756c54b3028
jnhalldev <jnhall.audio@gmail.com>
2023-01-23 10:47:07 -0500

    WorkExperience and Resume Classes completed.

--- app/src/main/java/com/example/crunchwrapsupreme/Resume.java
+++ app/src/main/java/com/example/crunchwrapsupreme/Resume.java
@@ -1,7 +1,33 @@
 package com.example.crunchwrapsupreme;
 
+import java.util.ArrayList;
+
 public class Resume {
-    String firstName;
-    String lastName;
-    String Bio;
+    private String bio;
+    private ArrayList<WorkExperience> experience;
+
+    Resume(String bio, ArrayList<WorkExperience> experience) {
+        this.bio = bio;
+        this.experience = experience;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setExperience(ArrayList<WorkExperience> experience) {
+        this.experience = experience;
+    }
+
+    public ArrayList<WorkExperience> getExperience() {
+        return experience;
+    }
+
+    public void removeWorkExperienceAtIndex(int index) {
+        experience.remove(index);
+    }
 }
--- app/src/main/java/com/example/crunchwrapsupreme/WorkExperience.java
+++ app/src/main/java/com/example/crunchwrapsupreme/WorkExperience.java
@@ -1,12 +1,98 @@
 package com.example.crunchwrapsupreme;
 
+import java.util.Calendar;
+
 public class WorkExperience {
-    String organizationName;
-    String organizationURL;
-    String city;
-    String state;
-    String positionTitle;
-    String beginningDate;
-    String endingDate;
-    String[] skillsUtilized;
+    private String organizationName;
+    private String organizationURL;
+    private String city;
+    private String state;
+    private String positionTitle;
+    private Calendar beginDate;
+    private Calendar endDate;
+    private String[] skillsUtilized;
+    boolean isEmployerVerified;
+
+    WorkExperience(String orgName,String orgURL, String city, String state, String position, Calendar begin, Calendar end, String[] skills) {
+        organizationName = orgName;
+        organizationURL = orgURL;
+        this.city = city;
+        this.state = state;
+        positionTitle = position;
+        beginDate = begin;
+        endDate = end;
+        skillsUtilized = skills;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public String getOrganizationURL() {
+        return organizationURL;
+    }
+
+    public void setOrganizationURL(String organizationURL) {
+        this.organizationURL = organizationURL;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public Calendar getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Calendar beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Calendar getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Calendar endDate) {
+        this.endDate = endDate;
+    }
+
+    public String[] getSkillsUtilized() {
+        return skillsUtilized;
+    }
+
+    public void setSkillsUtilized(String[] skillsUtilized) {
+        this.skillsUtilized = skillsUtilized;
+    }
+
+    public boolean isEmployerVerified() {
+        return isEmployerVerified;
+    }
+
+    public void setEmployerVerified(boolean employerVerified) {
+        isEmployerVerified = employerVerified;
+    }
 }
