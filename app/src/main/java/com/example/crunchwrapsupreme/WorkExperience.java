0ac4183e7135446c3cddd7222adba0958a2d658a
jnhalldev <jnhall.audio@gmail.com>
2023-01-22 23:33:42 -0500

    WorkExperience fields begin

--- app/src/main/java/com/example/crunchwrapsupreme/WorkExperience.java
+++ app/src/main/java/com/example/crunchwrapsupreme/WorkExperience.java
@@ -1,4 +1,12 @@
 package com.example.crunchwrapsupreme;
 
 public class WorkExperience {
+    String organizationName;
+    String organizationURL;
+    String city;
+    String state;
+    String positionTitle;
+    String beginningDate;
+    String endingDate;
+    String[] skillsUtilized;
 }
