7eb752126a129ce8e74a8e0156f03143abfafd5b
jnhalldev <jnhall.audio@gmail.com>
2023-02-22 16:14:04 -0500

    Visit Other Profile XML built out

---
+++ app/src/main/java/com/example/crunchwrapsupreme/VisitOtherProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.crunchwrapsupreme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisitOtherProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit_other_profile);
+    }
+}
